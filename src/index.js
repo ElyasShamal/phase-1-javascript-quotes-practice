47e85bad1f0c78d3ea34db0b0788483e49c4872d
shamalelyas <shamalm3398@connect.durhamtch.edu>
2024-06-24 02:07:32 -0400

    add edit button project completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const quoteList = document.getElementById("quote-list");
   const newQuoteForm = document.getElementById("new-quote-form");
-  const newQuote = document.getElementById("new-quote");
-  const author = document.getElementById("author");
+  const newQuoteInput = document.getElementById("new-quote");
+  const authorInput = document.getElementById("author");
   // Fetch request
   fetch("http://localhost:3000/quotes?_embed=likes")
     .then((response) => response.json())
@@ -13,6 +13,29 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
+  //Post request
+  newQuoteForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newQuote = {
+      quote: newQuoteInput.value,
+      author: authorInput.value,
+    };
+
+    fetch("http://localhost:3000/quotes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newQuote),
+    })
+      .then((response) => response.json())
+      .then((quote) => {
+        quote.likesCount = 0;
+        renderQuote(quote);
+      });
+
+    newQuoteForm.reset();
+  });
+
+  //display quotes
   function renderQuote(quote) {
     const li = document.createElement("li");
     li.className = "quote-card";
@@ -23,11 +46,18 @@ document.addEventListener("DOMContentLoaded", () => {
         <br>
         <button class='btn-success'>Likes: <span>${quote.likesCount}</span></button>
         <button class='btn-danger'>Delete</button>
+          <button class='btn-edit'>Edit</button>
+        <form class='edit-form' style='display: none;'>
+          <input type='text' class='edit-quote' value='${quote.quote}'>
+          <input type='text' class='edit-author' value='${quote.author}'>
+          <button type='submit'>Save</button>
+        </form>
       </blockquote>
     `;
 
     quoteList.appendChild(li);
 
+    // update the likes
     const likeButton = li.querySelector(".btn-success");
     likeButton.addEventListener("click", () => {
       fetch("http://localhost:3000/likes", {
@@ -39,6 +69,46 @@ document.addEventListener("DOMContentLoaded", () => {
         const likesSpan = likeButton.querySelector("span");
         likesSpan.textContent = quote.likesCount;
       });
+    });
+
+    // delete spacific quote
+    const deleteButton = li.querySelector(".btn-danger");
+    deleteButton.addEventListener("click", () => {
+      fetch(`http://localhost:3000/quotes/${quote.id}`, {
+        method: "DELETE",
+      }).then(() => li.remove());
+    });
+
+    // edit section
+    const editButton = li.querySelector(".btn-edit");
+    const editForm = li.querySelector(".edit-form");
+    const editQuoteInput = editForm.querySelector(".edit-quote");
+    const editAuthorInput = editForm.querySelector(".edit-author");
+
+    editButton.addEventListener("click", () => {
+      editForm.style.display =
+        editForm.style.display === "none" ? "block" : "none";
+    });
+
+    editForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const updatedQuote = {
+        quote: editQuoteInput.value,
+        author: editAuthorInput.value,
+      };
+
+      fetch(`http://localhost:3000/quotes/${quote.id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updatedQuote),
+      })
+        .then((response) => response.json())
+        .then((updated) => {
+          quote.quote = updated.quote;
+          quote.author = updated.author;
+          renderQuote();
+        });
     });
   }
 });
